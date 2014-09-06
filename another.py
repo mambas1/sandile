323ed277563f2adea417e18dae6ec1836d5767df
sandile <sandile@aims.edu.gh>
2014-09-06 20:50:47 +0000

    u

--- another.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'sandile'
-
-for i in range(1, 10):
-    tot += i
-    print(i)
-    print(tot)
--- delete.py
+++
@@ -1,9 +0,0 @@
-for i in range(1, 5):
-        s += 4*f(x[2*i - 1])
-        v += 2*f(x[2*i - 2])
-    return l + h + s + v
-if __name__ == "__main__":
-
-    def f(x):
-        return x
-    print(simpson_rule(f, 0, 2, 10))
